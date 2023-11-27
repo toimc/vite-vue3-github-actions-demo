ebed1a32f3b6f4b0b756c0086fed9df9781e3785
toimc <brian@toimc.com>
2023-11-28 00:10:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,18 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/vite-vue3-github-actions-demo/',
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode} => {
+  const isProd = mode === 'production'
+  return {
+    base: isProd ? '/vite-vue3-github-actions-demo/' : './',
+    plugins: [
+      vue(),
+      vueJsx(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
-})
+}))
